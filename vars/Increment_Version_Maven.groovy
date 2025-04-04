24dd1b542fcf5e694b7c452cf533b311f152ed66
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-04-04 13:45:34 -0700

    Update Jenkinsfile

--- vars/Increment_Version_Maven.groovy
+++ vars/Increment_Version_Maven.groovy
@@ -1,4 +1,4 @@
-def call(String major, String minor, String incremental) {
+def call() {
     echo 'Increment App Version ...'
     sh "mvn build-helper:parse-version versions:set \
         -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
