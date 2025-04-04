1ce46f736167afba2a50c68d8abb285c79e610b0
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-04-04 13:37:28 -0700

    Update Jenkinsfile

--- vars/Increment_Version_Maven.groovy
+++ vars/Increment_Version_Maven.groovy
@@ -1,7 +1,7 @@
 def call(String major, String minor, String incremental) {
     echo 'Increment App Version ...'
     sh "mvn build-helper:parse-version versions:set \
-        -DnewVersion=${major}.${minor}.${incremental} \
+        -DnewVersion=\\${major}.\\${minor}.\\${incremental} \
          versions:commit"
 
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
