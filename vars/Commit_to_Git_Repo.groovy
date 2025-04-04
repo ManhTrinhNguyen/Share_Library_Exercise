2e95319eb1a67eaca5e431ed0b356ac8495c3933
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-04-04 13:48:30 -0700

    Update Jenkinsfile

---
+++ vars/Commit_to_Git_Repo.groovy
@@ -0,0 +1,15 @@
+#!/user/bin/env groovy
+
+def call (String branch){
+    withCredentials([
+            usernamePassword(credentialsId: 'Github_Credential', usernameVariable: 'USER', passwordVariable: 'PWD')
+    ]) {
+        sh 'git config --global user.email "jenkin@gmail.com"'
+        sh 'git config --global user.name "Jenkin"'
+
+        sh "git remote set-url origin https://${USER}:${PWD}@github.com/ManhTrinhNguyen/Jenkin-Exercise-Java-Maven.git"
+        sh 'git add .'
+        sh 'git commit -m "ci: version bump"'
+        sh "git push origin HEAD:${branch}"
+    }
+}
