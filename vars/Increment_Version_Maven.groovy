0e4c18f57e036e51c1bb574cb3b2218ad21a489f
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-04-04 13:29:02 -0700

    Update Jenkinsfile

---
+++ vars/Increment_Version_Maven.groovy
@@ -0,0 +1,10 @@
+def call() {
+    echo 'Increment App Version ...'
+    sh 'mvn build-helper:parse-version versions:set \
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+         versions:commit'
+
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "java-maven-$version-$BUILD_NUMBER"
+}
