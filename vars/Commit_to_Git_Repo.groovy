a18304f46f2aae75b882446d696613cc5e531c28
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-05-10 11:23:39 -0700

    Add Commit_to_Git_Repo function

--- vars/Commit_to_Git_Repo.groovy
+++ vars/Commit_to_Git_Repo.groovy
@@ -2,14 +2,18 @@
 
 def call (String BRANCH_NAME){
     withCredentials([
-            usernamePassword(credentialsId: 'Github_Credential', usernameVariable: 'USER', passwordVariable: 'PWD')
-    ]) {
-        sh 'git config --global user.email "jenkin@gmail.com"'
-        sh 'git config --global user.name "Jenkin"'
+        usernamePassword(credentialsId: 'Github_Credential', usernameVariable: 'USER', passwordVariable: 'PWD')
+    ]){
+    // To set configuration that kept in .git folder and global configuration in git .
+    // I want to set git config Global I can put a flag --global
+    sh 'git config --global user.email "jenkin@gmail.com"' // If there is no User Email at all, Jenkin will complain when commiting changes . It will say there is no email that was detected to attach to as a metadata to that commit
+    sh 'git config --global user.name "Jenkins"'
 
-        sh "git remote set-url origin https://${USER}:${PWD}@github.com/ManhTrinhNguyen/Jenkin-Exercise-Java-Maven.git"
-        sh 'git add .'
-        sh 'git commit -m "ci: version bump"'
-        sh "git push origin HEAD:${BRANCH_NAME}"
+    // Set Origin access
+    sh "git remote set-url origin https://${USER}:${PWD}@${GIT_URL}"
+
+    sh "git add ."
+    sh 'git commit -m "ci: version bump"'
+    sh "git push origin HEAD:${BRANCH_NAME}"
     }
 }
