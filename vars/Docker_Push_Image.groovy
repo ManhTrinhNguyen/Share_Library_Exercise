d46eab83ef9407fcf5dab02c4aac4f07ff471e9c
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-05-10 11:12:43 -0700

    Add Docker_Login_ECR function

--- vars/Docker_Push_Image.groovy
+++ vars/Docker_Push_Image.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
 def call () {
-    echo "Push Image to Docker Hub"
-    sh "docker push ${DOCKER_REPO}:${IMAGE_NAME}"
+    sh "docker push ${IMAGE_NAME}"
+    echo "Push Image Success ....."
 }
