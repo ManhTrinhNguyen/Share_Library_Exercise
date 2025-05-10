116a5fc1efa7783d230ee3fd622c284b67ba1445
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-05-10 10:16:13 -0700

    Update Incremnet_Version_gradle

--- vars/Increment_Version_Gradle.groovy
+++ vars/Increment_Version_Gradle.groovy
@@ -1,7 +1,7 @@
-def call () {
+def call (String Update_Version) {
   echo "Increment App Version"
 
-  sh "gradle patchVersionUpdate"   // To Increase Major Version : gradle majorVersionUpdate,  To Increase Minor Version : gradle minorVersionUpdate , To Increase Patch Version : gradle patchVersionUpdate
+  sh "gradle ${Update_Version}"   // To Increase Major Version : gradle majorVersionUpdate,  To Increase Minor Version : gradle minorVersionUpdate , To Increase Patch Version : gradle patchVersionUpdate
 
 
   def version = readProperties(file: 'version.properties')
